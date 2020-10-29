a099d93cc5bbf5413cfcea20426cda57b69f9e7f
Eric Muir <eric@ebdm.dev>
2020-10-29 15:51:00 +0000

    Added multiple buildspec

--- codebuild.tf
+++ codebuild.tf
@@ -20,13 +20,54 @@ resource "aws_codebuild_project" "plan" {
 
   logs_config {
     cloudwatch_logs {
-      group_name  = "franscape-instance-plan"
+      group_name  = "franscape-instance"
       stream_name = var.plan_name
     }
   }
 
   source {
     type = "CODEPIPELINE"
+    buildspec = "buildspec.plan.yaml"
+  }
+
+  source_version = "refs/heads/master"
+
+  tags = {
+    Name     = var.build_description
+    Project  = "franscape"
+  }
+}
+
+resource "aws_codebuild_project" "apply" {
+  name = var.apply_name
+  description = var.build_description
+
+  build_timeout = "5"
+  service_role  = aws_iam_role.build.arn
+
+  artifacts {
+    type = "S3"
+    location = var.output_bucket
+    name = var.output_key
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "franscape-instance"
+      stream_name = var.apply_name
+    }
+  }
+
+  source {
+    type = "CODEPIPELINE"
+    buildspec = "buildspec.apply.yaml"
   }
 
   source_version = "refs/heads/master"
