2c7daa1ecefe5325b224618562f1cdb3bb050f01
Eric Muir <eric@ebdm.dev>
2020-10-30 11:45:51 +0000

    Update missing variable

--- codebuild.tf
+++ codebuild.tf
@@ -1,7 +1,7 @@
 data "template_file" "plan_spec" {
   template = file("${path.module}/files/buildspec.plan.yaml.tpl")
   vars = {
-    id = var.repo_name,
+    id = "${var.system_id}_repository",
     bucket = aws_s3_bucket.output_bucket.bucket
   }
 }
